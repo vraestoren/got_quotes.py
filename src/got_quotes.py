d4df8b3359da4939f357c00e8dc3baaf253f2a1d
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-15 12:57:47 +0300

    Refactor API calls to use _get method

--- src/got_quotes.py
+++ src/got_quotes.py
@@ -8,28 +8,28 @@ class GameOfThronesQuotes:
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 			"accept": "application/json"
 		}
-	
+
+	def _get(self, endpoint: str) -> dict:
+		return self.session.get(endpoint).json()
+
 	def get_random_quote(self, number: int = 1) -> dict:
-		return self.session.get(
-			f"{self.api}/random/{number}").json()
+		return self._get(f"/random/{number}")
 	
 	def get_character_quotes(
 			self,
 			character_name: str,
 			number: int = 1) -> dict:
-		return self.session.get(
-			f"{self.api}/author/{character_name}/{number}").json()
+		return self._get(
+			f"/author/{character_name}/{number}")
 	
 	def get_houses_list(self) -> dict:
-		return self.session.get(f"{self.api}/houses").json()
+		return self._get("/houses")
 
 	def get_house_by_name(self, house_name: str) -> dict:
-		return self.session.get(
-			f"{self.api}/house/{house_name}").json()
+		return self._get(f"/house/{house_name}")
 	
 	def get_characters_list(self) -> dict:
-		return self.session.get(f"{self.api}/characters").json()
+		return self._get("/characters")
 	
 	def get_character_details(self, character_name: str) -> dict:
-		return self.session.get(
-			f"{self.api}/characters/{character_name}").json()
+		return self._get(f"/characters/{character_name}")
